cfca5a6240a6db034aaf29375d2d88bc1b855118
Shota Tevdorashvili <shotatevdorashvilibusiness@gmail.com>
2025-03-02 14:48:32 +0400

    styling

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import * as THREE from 'three';
+import './style.css'
 
 // Scene
 const scene = new THREE.Scene();
 
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+}
+
 // Camera
 const camera = new THREE.PerspectiveCamera(
   75,
-  800 / 600,
+  sizes.width/sizes.height,
   0.1,
   1000
 );
@@ -14,7 +21,7 @@ camera.position.z = 3;
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(800, 600);
+renderer.setSize(sizes.width, sizes.height);
 document.body.appendChild(renderer.domElement);
 
 // Cube Geometry and Material
