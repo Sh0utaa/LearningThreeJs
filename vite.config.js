60259d9abeb7b9649d025479adb954844c073761
Shota Tevdorashvili <shotatevdorashvilibusiness@gmail.com>
2025-03-07 22:56:39 +0400

    I'm not a real man anymore, testing in production is over

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-export default {
-    server: {
-      host: '0.0.0.0', // Listen on all network interfaces
-      port: 3000, // Optional: Specify a port
-    },
-  };
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    host: '0.0.0.0', // Listen on all network interfaces
+    port: 3000, // Optional: Specify a port
+    
+    allowedHosts: [
+      '879a-149-3-101-219.ngrok-free.app', // Add your ngrok host here
+    ],
+  },
+});
