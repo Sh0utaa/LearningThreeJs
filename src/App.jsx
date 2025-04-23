04ccd1c7921c208d7df51377005da393f1d0d61d
Shota Tevdorashvili <shotatevdorashvilibusiness@gmail.com>
2025-04-23 15:43:26 +0400

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 // Import your components
-import TaskST011 from './TaskST011/TaskST011'
-import TaskST012 from './TaskST012/TaskST012'
-import TaskST013 from './TaskST013/TaskST013'
-import TaskST014 from "./TaskST014/TaskST014"
-// Add more as needed
+import TaskST011 from './TaskST011/TaskST011';
+import TaskST012 from './TaskST012/TaskST012';
+import TaskST013 from './TaskST013/TaskST013';
+import TaskST014 from './TaskST014/TaskST014';
 
 const App = () => (
   <BrowserRouter>
@@ -16,10 +15,23 @@ const App = () => (
       <Route path="/task012" element={<TaskST012 />} />
       <Route path="/task013" element={<TaskST013 />} />
       <Route path="/task014" element={<TaskST014 />} />
-      {/* Add more routes here */}
-      <Route path="*" element={<div>404 - Not Found</div>} />
+      <Route
+        path="*"
+        element={
+          <div>
+            <nav>
+              <ul>
+                <li><Link to="/task011">Task 011</Link></li>
+                <li><Link to="/task012">Task 012</Link></li>
+                <li><Link to="/task013">Task 013</Link></li>
+                <li><Link to="/task014">Task 014</Link></li>
+              </ul>
+            </nav>
+          </div>
+        }
+      />
     </Routes>
   </BrowserRouter>
-)
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
